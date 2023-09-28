a1cf08d79e3a18aec83b53dbc9d3a6ea6625f9c3
Jin Young Jang - MyPC <ending1025@gmail.com>
2023-09-29 08:54:00 +0900

    Drill 6 제출 (2018184042 장진영)
    character run to arrow
    Add Character Animation Idle

--- 2DGame_Drill6.py
+++ 2DGame_Drill6.py
@@ -9,7 +9,7 @@ tuk_Ground = load_image("TUK_GROUND.png")
 
 index, t = 0 ,0
 select = -1
-motion = 0
+motion, tempmotion = 2, 0
 playerX, playerY = tuk_width  // 2, tuk_height // 2
 route = list()
 running = True
@@ -20,41 +20,73 @@ def draw_arrow():
         hand_arrow.draw(routeX,routeY)
 
 def player_move():
-    global playerX, playerY, route, t, select
-    routeX, routeY = route[0]
-    playerX = (1-t) * playerX + t * routeX
-    playerY = (1-t) * playerY + t * routeY
-    t += 0.05
-    if playerX == routeX:
-        t = 0
-    
+    global playerX, playerY, route, t, select, index
+    if(select >= 0):  
+
+       routeX, routeY = route[0]
+       playerX = (1-t) * playerX + t * routeX
+       playerY = (1-t) * playerY + t * routeY
+       t += 0.05
+       if playerX == routeX:
+          t = 0
+          del route[0]
+          select -= 1
+          index = 0
+
 def player_motion():
-    global motion, route
-    routeX, routeY = route[0]
-    if(playerX > routeX):
-        motion = 0
-    elif(playerX < routeX):
-        motion = 1
+    global motion, tempmotion,  route
+    if(select >= 0):  
+
+        routeX, routeY = route[0]
+        if(playerX > routeX):
+            motion = 0
+            tempmotion = motion
+        elif(playerX < routeX):
+            motion = 1
+            tempmotion = motion
+ 
+    elif(select == -1):
+        if tempmotion == 0:
+             motion = 2
+        elif tempmotion == 1:
+             motion = 3
+
+def play_animation():
+    if motion == 0 or motion == 1:
+       player_run()
+    else:
+       player_idle()
 
 def player_run():
     global index
     frame = [(27,683-428,40,60), (71, 683-428,41,60), (123,683-428,38,60), 
     (173,683-428,33,60), (213,683-428,46,60), (261,683-428,41,60), 
     (312,683-428,38,60), (358,683-428,32,60)]
-   	
-    
+   	    
     draw(frame)
 
     index += 1
     if(index == 8):
     	index = 0
 
+def player_idle():
+    global index
+    frame = frame = [(32, 683-72,39,60), (83, 683-72,39,60),
+     (132, 683-72,39,60), (182, 683-72,39,60)]
+
+       
+    draw(frame)
+
+    index += 1
+    if(index == 4):
+        index = 0
+
 def draw(frame):
     frameX, frameY, width, height = frame[index]
-    if motion == 0:
+    if motion == 0 or motion == 2:
        player.clip_composite_draw(frameX,frameY,width,height, 0, 'h', playerX, playerY, 100,150)
 
-    if motion == 1:
+    if motion == 1 or motion == 3:
        player.clip_draw(frameX,frameY, width, height, playerX, playerY, 100 , 150)
 
 def handle_events():
@@ -76,16 +108,14 @@ while running:
     clear_canvas()
     tuk_Ground.draw(tuk_width//2,tuk_height//2)
 
-    player_run()
+    play_animation()
     draw_arrow()
     update_canvas()
     
     handle_events()
-    if(select >= 0):     
-       player_move()
-       player_motion()
-      
-    
+    player_move()
+    player_motion()  
+
     delay(0.05)
 
     if not running: 
